b17f46e93c5ce3a665247c4bdf15ea2ec55e18e9
Berens,Nathaniel <nrberens@gmail.com>
2016-11-10 17:27:54 -0500

    Added music and tweaked gem collection code

--- Assets/Scripts/Gem.cs
+++ Assets/Scripts/Gem.cs
@@ -4,22 +4,42 @@ using System.Collections;
 public class Gem : MonoBehaviour {
 
 	public GameController gc;
+	public AudioClip chime;
+	public AudioSource audioSource;
+	[SerializeField] private float speed;
 	// Use this for initialization
 	void Start () {
 		gc = GameObject.Find("GameController").GetComponent<GameController>();
+		audioSource = GetComponent<AudioSource>();
+		audioSource.clip = chime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
 	void OnTriggerEnter(Collider collider) {
+		if(collider.CompareTag("Player")) {
 			//decrement gem count
-			gc.gemsRemaining--;
-			//TODO suck gem into player?
-			//Play sound effect?
-			//destroy the gem
-			GameObject.Destroy(this.gameObject);
+			StartCoroutine(CollectGemAndDestroy(collider.gameObject));
+		}
+	}
+
+	public IEnumerator CollectGemAndDestroy(GameObject obj) {
+		//Assumes the player is the only other collider
+		//TODO suck gem into player?
+		//Play sound effect?
+		//destroy the gem
+		GetComponent<Renderer>().enabled = false;
+		GetComponent<Collider>().enabled = false;
+
+		gc.gemsRemaining--;
+		audioSource.PlayOneShot(chime);
+		while(audioSource.isPlaying) {
+			yield return null;
+		}
+
+		GameObject.Destroy(this.gameObject);
 	}
 }
